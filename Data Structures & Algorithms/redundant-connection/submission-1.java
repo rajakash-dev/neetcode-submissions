5ae950e47f141f973994bb34ca722d8968e83bd1
Akash Raj <akashraj8@gmail.com>
2026-07-21 09:17:46 +0530

    Add: redundant-connection - submission-1

---
+++ Data Structures & Algorithms/redundant-connection/submission-1.java
@@ -0,0 +1,60 @@
+class Solution {
+    public int[] findRedundantConnection(int[][] edges) {
+        int n = edges.length;
+        UnionFind uf = new UnionFind(n);
+        for (int[] edge : edges) {
+            if (uf.connected(edge[0] - 1, edge[1] - 1)) {
+                return edge;
+            }
+            uf.union(edge[0] - 1, edge[1] - 1);
+        }
+        return new int[] {};
+    }
+
+}
+
+// UnionFind.class
+class UnionFind {
+    private final int[] root;
+    // Use a rank array to record the height of each vertex, i.e., the "rank" of each vertex.
+    private final int[] rank;
+
+    public UnionFind(int size) {
+        root = new int[size];
+        rank = new int[size];
+        for (int i = 0; i < size; i++) {
+            root[i] = i;
+            rank[i] = 1; // The initial "rank" of each vertex is 1, because each of them is
+            // a standalone vertex with no connection to other vertices.
+        }
+    }
+
+    // The find function here is the same as that in the disjoint set with path compression.
+    public int find(int x) {
+        if (x == root[x]) {
+            return x;
+        }
+        // Some ranks may become obsolete so they are not updated
+        return root[x] = find(root[x]);
+    }
+
+    // The union function with union by rank
+    public void union(int x, int y) {
+        int rootX = find(x);
+        int rootY = find(y);
+        if (rootX != rootY) {
+            if (rank[rootX] > rank[rootY]) {
+                root[rootY] = rootX;
+            } else if (rank[rootX] < rank[rootY]) {
+                root[rootX] = rootY;
+            } else {
+                root[rootY] = rootX;
+                rank[rootX] += 1;
+            }
+        }
+    }
+
+    public boolean connected(int x, int y) {
+        return find(x) == find(y);
+    }
+}
